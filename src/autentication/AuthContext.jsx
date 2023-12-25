e6cb3a6a3b1e522e670d7605f2d8d2b52dc20152
jhonyarias69 <mjosearboledaguerra@gmail.com>
2023-12-25 18:04:48 -0500

    se agregan datos faltantes para luego mostrar el usuario conectado

--- src/autentication/AuthContext.jsx
+++ src/autentication/AuthContext.jsx
@@ -1,22 +1,26 @@
 import { createContext, useContext, useState } from 'react';
+import { loginUsers } from '../lib/Api';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
 
-  const login = () => {
+  const login = (userData) => {
     // Lógica para autenticar al usuario
     setIsAuthenticated(true);
+    setUser(userData);
   };
 
   const logout = () => {
     // Lógica para cerrar sesión
     setIsAuthenticated(false);
+    setUser(null);
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+    <AuthContext.Provider value={{ isAuthenticated, user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
