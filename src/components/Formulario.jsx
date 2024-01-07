ca5ba07bce3865b8dd9f1d71ea5962f2637bcd98
jhonyarias69 <mjosearboledaguerra@gmail.com>
2024-01-07 17:41:15 -0500

    se agregan librerias, se organiza el componente Formulario para que capture pdf y se le agrega format para organizar la fecha

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,7 +3,9 @@ import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 import "../css/index.css";
 import { registerDocument } from "../lib/Api";
-import {toast} from "react-toastify"
+import {toast} from "react-toastify";
+import { useAuth } from '../autentication/AuthContext';
+import { format } from "date-fns";
 
 
 const Formulario = () => {
@@ -13,29 +15,45 @@ const Formulario = () => {
     formState: { errors },
   } = useForm();
 
+  const { isAuthenticated, user, logout } = useAuth();
+
   const redireccion= useNavigate();
 
   const onSubmit = handleSubmit(async (values) => {
-    try{
-        const submit = await registerDocument(values)
-
-        if (submit) {
-          toast.success('Documento Registrado Exitosamente');
-          redireccion("/menu");
-        }
+    try {
+      const formData = new FormData();
+      formData.append("nombre", values.nombre);
+      formData.append("fechaRadicacion", values.fechaRadicacion);
+      formData.append("dependencia", values.dependencia);
+      formData.append("asunto", values.asunto);
+      formData.append("documentoPDF", values.documentoPDF[0]);
+  
+      const submit = await registerDocument(formData);
+      
+      const formattedDate = format(new Date(values.fechaRadicacion), "yyyy-MM-dd");
+      formData.append("fechaRadicacion", formattedDate); //combierte el formato de fecha que se ingrese en el formato que requiere Mysql
+  
+      if (submit) {
+        const data = await response.json();
+        toast.success('Documento Registrado Exitosamente');
+        redireccion("/menu");
+      } else {
+        toast.error('Error al registrar su solicitud');
+      }
     } catch (error) {
       console.log("Se produjo un error al registrar la solicitud", error);
       toast.error('Error al registrar su solicitud');
     }
-
   });
+  
 
   return (
     <div className="container1">
       <h2 className="titulo1">Solicitud de Documento</h2>
+      <h2 className="titulo">{user.nombre}</h2>
       <div className="containerf">
       <form className="form1" onSubmit={onSubmit}>
-        <label className="label1" htmlFor="nombre">Nombre de Usuario</label>
+        <label className="titulo1" htmlFor="nombre">Nombre de Usuario</label>
         <input className="input1"
           type="text"
           {...register("nombre", {
@@ -45,11 +63,11 @@ const Formulario = () => {
             },
           })}
         />
-        {errors.nombreUsuario && (
+        {errors.nombre && (
           <span>{errors.nombreUsuario.message}</span>
         )}
          
-         <label className="label1" htmlFor="dependencia">Dependencia</label>
+         <label className="titulo1" htmlFor="dependencia">Dependencia</label>
          <select className="select1" {...register("dependencia", { required: true })}>
             <option value="1">Hacienda Municipal</option>
             <option value="2">Planeación y Desarrollo Urbano</option>
@@ -64,7 +82,7 @@ const Formulario = () => {
             <span>{errors.dependenciaResponsable.message}</span>
         )}
 
-        <label className="label1" htmlFor="fechaRadicacion">Fecha de Radicación</label>
+        <label className="titulo1" htmlFor="fechaRadicacion">Fecha de Radicación</label>
         <input className="input1"
           type="text"
           {...register("fechaRadicacion", {
@@ -78,7 +96,7 @@ const Formulario = () => {
           <span>{errors.fechaSolicitud.message}</span>
         )}
 
-        <label className="label1" htmlFor="asunto">Asunto</label>
+        <label className="titulo1" htmlFor="asunto">Asunto</label>
         <textarea className="textarea1"
           {...register("asunto", {
             required: {
@@ -91,7 +109,7 @@ const Formulario = () => {
           <span>{errors.asunto.message}</span>
         )}
 
-        <label className="label1" htmlFor="documentoPDF">Adjuntar Documento (PDF)</label>
+        <label className="titulo1" htmlFor="documentoPDF">Adjuntar Documento (PDF)</label>
         <input className="input1"
           type="file"
           accept=".pdf"
